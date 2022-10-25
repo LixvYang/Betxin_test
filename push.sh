3b2632302d6d40ccaf67fe0a37eefb9cb2ad5d6f
LixvYang <2690688423@qq.com>
2022-10-25 10:34:31 +0800

    update

--- push.sh
+++ push.sh
@@ -1,16 +1,3 @@
 git add .
 git commit -m "update"
 git push
-
-
-git clone https://github.com/LixvYang/Betxin.git BetxinCopy
-rm -rf Betxin/
-mv BetxinCopy/ Betxin/
-cp -r config/ Betxin/Backend/
-mkdir /Betxin/Backend/log
-cd Betxin/Backend/
-
-echo "请关闭8080端口 lsof -i:8080"
-echo "并再次启用8080"
-echo "go run main.go &"
-~                         
